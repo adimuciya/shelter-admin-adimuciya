10e2682c091f3b9226e6e1dfa5cfa7a127ef80b4
dasha <frgyvu@gmail.com>
2020-07-16 18:26:26 +0300

    ++

--- src/views/Animals.vue
+++ src/views/Animals.vue
@@ -1,34 +1,12 @@
 <template>
     <section>
         <h2>Список животных</h2>
-        <div v-for="(animal, i) in animals" :key="i">
-            <h2>{{animal.animal_name}}</h2>
-            <p>Возраст: {{ animal.age }}</p>
-            <p>Категория: {{ animal.description }}</p>
-            <hr>
-        </div>
-
     </section>
 </template>
 
 <script>
-    import axios from "axios";
-
     export default {
-        name: "Animals",
-        data: function() {
-            return {
-                animals: []
-            }
-        },
-        created() {
-            axios
-                .get('http://shelter/api/animals')
-                .then(response => {
-                    this.animals = response.data;
-                    console.log(this.animals)
-                });
-        }
+        name: "Animals"
     }
 </script>
 
