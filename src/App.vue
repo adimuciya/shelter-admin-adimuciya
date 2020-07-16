b4883e51beb5b97016e6b7721ae65cbdc6f61a32
dasha <frgyvu@gmail.com>
2020-07-16 18:13:19 +0300

    ++

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/add/animal">Добавить животное</router-link> |
+      <router-link to="/animals">Список животных</router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/views/NewAnimal.vue
@@ -0,0 +1,40 @@
+<template>
+    <section>
+        <h2>Добавление нового животного</h2>
+        <form @submit.prevent="addAnimal">
+            <input type="text" v-model.trim="name">
+            <input type="submit" value="Добавить">
+        </form>
+    </section>
+</template>
+
+<script>
+    import axios from "axios";
+
+    export default {
+        name: "NewAnimal",
+        data: function() {
+            return {
+                name: ''
+            }
+        },
+        methods: {
+            addAnimal: function () {
+                const fd = new FormData();
+                fd.append('name', this.name);
+                console.log(this.name);
+                axios
+                    .post('http://shelter/api/add/animal', fd)
+                    .then(response => {
+                        console.log(response)
+                    });
+            }
+        }
+
+    }
+
+</script>
+
+<style scoped>
+
+</style>
